87733ab9fceed18dfc734a876d4c20115569d2e5
Balaji-200 <60106901+Balaji-200@users.noreply.github.com>
2020-09-09 14:51:24 +0530

    Update config.js
    removed keys

--- src/config.js
+++ src/config.js
@@ -4,13 +4,13 @@ const absolutePath = path.resolve(__dirname,"../bin/certificate.pem");
 const publicKey = fs.readFileSync(absolutePath, 'utf8');
 module.exports = {
     secretKey: publicKey,
-    mongoUrl: "mongodb://balaji:#12Aspirine@35.154.92.66/Quiz-Server",
+    mongoUrl: "YOUR URL",
     facebookIds:{
-        clientId: "2829468820667711",
-        clientSecret:"234697364f967fce70759afd9ba81bdf"
+        clientId: "YOUR ID",
+        clientSecret:"YOUR SECRET"
     },
     googleOauth2:{
-        clientId:"922500157871-7ggiid26hibtcuontr93e3cneb4to9a1.apps.googleusercontent.com",
-        clientSecret:"sdGerZMdA4LkLtb8MCnh6uQ0"
+        clientId:"YOUR ID",
+        clientSecret:"YOUR SECRET"
     }
 }
